dc82d7e9c2d79f251304b400dab3b33c8887af1b
Chase Marangu <cmarangu@vassar.edu>
2024-10-15 10:54:14 -0400

    Update my boilerplate code

--- script.js
+++ script.js
@@ -1,179 +1,136 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+/* TransformControls is for the red-green-blue arrows to move objects around the scene */
+import { TransformControls } from 'three/addons/controls/TransformControls.js';
 
 import { VRButton } from 'three/addons/webxr/VRButton.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import { ARButton } from 'three/addons/webxr/ARButton.js';
 
-function myPrint(txt) {
-	txt = "" + txt;
-	// Chase Note: might later make it so that it adds a "pop up box" with CSS animations to the <body> element that hovers over before floating away, like the canvas (instructure) error messages
-	let divEl = document.createElement("div")
-	// txt = txt.split("\t").join("<tab>")
-	// txt = txt.split("\t").join(" ")
-	txt = txt.split("\t").join("----")
-	let theLines = txt.split("\n")
-	for (let theLine of theLines) {
-		let childEl = document.createElement("div")
-		childEl.textContent = theLine;
-		childEl.style = "border:unset;" // override CSS <style> tags and linked stylesheet
-		// Element.append() (or Node.appendChild()) it as the last element in the body. Could also use Node.insertBefore().
-		divEl.append(childEl)
-	}
-	document.body.append(divEl)
+const canvasWrapperEl = document.querySelector('#canvas-wrapper');
+let renderer, scene, camera, controls;
+
+let chargedParticles = [];
+
+init();
+
+function init () {
+	// === scene initialization === //
+	camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+	// z-position of non-zero is needed for OrbitControls to work - MUSTDO
+	camera.position.set(0, 0, -10)
+	camera.position.y = 10;
+	scene = new THREE.Scene();
+	// uniforms = {
+	// 	pointTexture: { value: new THREE.TextureLoader().load( 'https://threejs.org/examples/textures/sprites/spark1.png' ) }
+	// };
+	// const shaderMaterial = new THREE.ShaderMaterial( {
+	// 	uniforms: uniforms,
+	// 	vertexShader: document.getElementById( 'vertexshader' ).textContent,
+	// 	fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+	// 	blending: THREE.AdditiveBlending,
+	// 	depthTest: false,
+	// 	transparent: true,
+	// 	vertexColors: true
+	// } );
+	// MUSTDO: ^
+	// mustdo - fix material below and add wireframe
+	scene.add( new THREE.Mesh(new THREE.BoxGeometry( 1, 1, 1 ), new THREE.MeshPhongMaterial( { color: 0x156289, emissive: 0x072534, side: THREE.DoubleSide, flatShading: true })) );
+	const lights = [];
+		lights[ 0 ] = new THREE.DirectionalLight( 0xffffff, 3 );
+		lights[ 1 ] = new THREE.DirectionalLight( 0xffffff, 3 );
+		lights[ 2 ] = new THREE.DirectionalLight( 0xffffff, 3 );
+		lights[ 0 ].position.set( 0, 200, 0 );
+		lights[ 1 ].position.set( 100, 200, 100 );
+		lights[ 2 ].position.set( - 100, - 200, - 100 );
+	scene.add( lights[ 0 ] ); scene.add( lights[ 1 ] ); scene.add( lights[ 2 ] );
+	scene.add( new THREE.GridHelper(10, 10) )
+	//
+	//
+	//
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	canvasWrapperEl.append(renderer.domElement)
+	renderer.xr.enabled = true;
+	// renderer.xr.setFramebufferScaleFactor(2.0); // increase resoluition - MUSTDO
+	document.body.appendChild( VRButton.createButton( renderer ) );
+	// renderer.xr.setReferenceSpaceType('local');
+	//
+	//
+	controls = new OrbitControls( camera, renderer.domElement );
+	controls.enableDamping = true;
+	controls.minDistance = 10;
+	controls.maxDistance = 50;
+	controls.target.set(0, 0, 0); // for VR?
+	controls.update()
+	//
+	// === function calls that get called once at the start === //
+	// ResizeObserver calls the function once automatically
+	// onWindowResize()
+	renderer.setAnimationLoop( animate )
 }
 
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(); // updated in ResizeObserver()
-
-const renderer = new THREE.WebGLRenderer();
-const theWrap = document.querySelector("#canvas-wrapper")
-// renderer.setSize( window.innerWidth/2, window.innerHeight/2);
-// Element.append() vs Node.appendChild().
-// renderer.domElement is the canvas.
-theWrap.append(renderer.domElement)
-
-
-document.body.appendChild( VRButton.createButton( renderer ) );
-renderer.xr.enabled = true;
-
-// https://github.com/mrdoob/three.js/blob/master/examples/webgpu_compute_particles.html
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.minDistance = 5;
-controls.maxDistance = 200;
-controls.target.set( 0, 0, 0 );
-controls.update();
-controls.saveState();
-
-
-// Geometry and materials
-let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-let cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-
-//create a blue LineBasicMaterial
-let material2 = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-let points2 = [];
-points2.push( new THREE.Vector3( - 1, 0, 0 ) );
-points2.push( new THREE.Vector3( 0, 1, 0 ) );
-points2.push( new THREE.Vector3( 1, 0, 0 ) );
-let geometry2 = new THREE.BufferGeometry().setFromPoints( points2 );
-let line2 = new THREE.Line( geometry2, material2 );
-scene.add(line2)
-
-
-
-// create a bunch of points, copied from ThreeJS docs
-// https://threejs.org/docs/#api/en/materials/PointsMaterial
-// array of particles
-let arr = [];
-
-for ( let i = 0; i < 1000; i ++ ) {
-	const { random } = Math;
-	const x = random() * 100 - 50;
-	const y = random() * 100 - 50;
-	const z = random() * 100 - 50;
-	
-	arr.push( x, y, z );
+function vecField (x, y, z) {
+	// sum up charged spheeres k*q/r
+	// varying radius and charge
 }
 
-const geometry3 = new THREE.BufferGeometry();
-geometry3.setAttribute( 'position', new THREE.Float32BufferAttribute( arr, 3 ) );
-const material3 = new THREE.PointsMaterial( { color: 0x888888 } );
-const points3 = new THREE.Points( geometry3, material3 );
-scene.add( points3 );
+// renderer.setAnimationLoop( animate );
 
+// function render () { renderer.render( scene, camera ); }
 
-// particles attempt, based on
-// https://github.com/mrdoob/three.js/blob/master/examples/webgpu_particles.html and
-// https://github.com/mrdoob/three.js/blob/master/examples/webgpu_compute_particles.html
-// which have a different importmap : "imports": { "three": "../build/three.webgpu.js" }
-// const smokeNodeMaterial = new THREE.SpriteMaterial();
-// smokeNodeMaterial.colorNode = mix( color( 0xf27d0c ), smokeColor, life.mul( 2.5 ).min( 1 ) ).mul( fakeLightEffect );
-// smokeNodeMaterial.opacityNode = opacityNode;
-// smokeNodeMaterial.positionNode = offsetRange.mul( lifeTime );
-// smokeNodeMaterial.scaleNode = scaleRange.mul( lifeTime.max( 0.3 ) );
-// smokeNodeMaterial.depthWrite = false;
-// smokeNodeMaterial.transparent = true;
-
-// const smokeInstancedSprite = new THREE.Mesh( new THREE.PlaneGeometry( 1, 1 ), smokeNodeMaterial );
-// smokeInstancedSprite.scale.setScalar( 400 );
-// smokeInstancedSprite.count = 2000;
-// scene.add( smokeInstancedSprite );
-
-
-
-
-
-
-let frameCount = 0;
-let canImmersiveXR = false;
-if ('xr' in navigator) {}
-let par1 = 0.2;
-
-
-function resetScene () {
-	par1 = 0;
-	cube.rotation.x = 0;
-	cube.rotation.y = 0;
-	// console.log(par1)
-	controls.reset();
-}
-function drawScene () {
+function animate() {
+	// const sizes = geometry.attributes.size.array;
+	// const pos = geometry.attributes.position.array;
+	// let vel;
+	// for ( let i = 0; i < particles; i ++ ) {
+	// 	// sizes[ i ] = 10 * ( 1 + Math.sin( 0.1 * i + time ) )
+	// 	sizes[ i ] = 10 * ( 1 )
+	// 	// sizes[ i ] = 10 * ( 10 )
+	// 	vel = v(pos[i*3], pos[i*3+1], pos[i*3+2])
+	// 	pos[i*3] += vel[0];
+	// 	pos[i*3 +1] += vel[1];
+	// 	pos[i*3 +2] += vel[2];
+	// 	pos[i*3]    = modClamp(pos[i*3+0], -radius, radius)
+	// 	pos[i*3 +1] = modClamp(pos[i*3+1], -radius, radius)
+	// 	pos[i*3 +2] = modClamp(pos[i*3+2], -radius, radius)
+	// }
+	// geometry.attributes.size.needsUpdate = true;
+	// geometry.attributes.position.needsUpdate = true;
 	renderer.render( scene, camera );
-	++frameCount;
+	controls.update();
 }
 
 
-function animScene () {
-	cube.rotation.x = par1 / 30 * Math.PI/2;
-	cube.rotation.y = par1 / 30 * Math.PI/2;
-	++par1;
-}
+// === Below I have put event listeners and stuff ==== //
 
-resetScene();
-drawScene();
-play()
-
-function updateUI () {
-	document.querySelector('#framecount-stat').textContent = 'frames: ' + frameCount;
-}
-let updateUIid = setInterval( updateUI, 100 )
-
-
-function animLoopFunc() {
-	drawScene();
-	animScene();
-}
-
-function play () {
-	renderer.setAnimationLoop(animLoopFunc);
-}
-document.querySelector('#play-button').addEventListener('click', play)
-
-function pause () {
-	renderer.setAnimationLoop( null )
-}
-document.querySelector('#pause-button').addEventListener('click', pause)
-
-function restart () {
-	pause()
-	frameCount = 0;
-	resetScene()
-	drawScene()
-}
-document.querySelector('#restart-button').addEventListener('click', restart)
-
+// window.addEventListener('resize',()=>{...})
 new ResizeObserver(function(){
-	camera.aspect = theWrap.clientWidth / theWrap.clientHeight;
+	camera.aspect = canvasWrapperEl.clientWidth / canvasWrapperEl.clientHeight;
 	camera.updateProjectionMatrix();
-	renderer.setSize(theWrap.clientWidth, theWrap.clientHeight)
-	// took me a while, but I figured out we
-	// need to drawScene() after doing this other
-	// stuff in the ResizeObserver to prevent
-	// the canvas from blinking white.
-	drawScene()
+	renderer.setSize(canvasWrapperEl.clientWidth, canvasWrapperEl.clientHeight)
+	renderer.render( scene, camera );
 }).observe(document.querySelector('#canvas-wrapper'))
+
+document.querySelector('#pause-button').addEventListener('click', ()=>{
+	renderer.setAnimationLoop( null );
+})
+
+document.querySelector('#play-button').addEventListener('click', ()=>{
+	renderer.setAnimationLoop( animate );
+})
+
+renderer.xr.addEventListener('sessionstart', (e) => {
+	// MUSTDO - clean up
+	// Copied from https://discourse.threejs.org/t/initial-webxr-position-same-as-camera-position/36682/13
+	// controls.update();
+	// const baseReferenceSpace = renderer.xr.getReferenceSpace();
+	// const offsetPosition = camera.position;
+	//const offsetRotation = camera.rotation;
+	// const offsetRotation = camera.quaternion;
+	// const transform = new XRRigidTransform( offsetPosition, { x: offsetRotation.x, y: -(offsetRotation.y), z: offsetRotation.z, w: offsetRotation.w } ); 
+	//const transform = new XRRigidTransform( offsetPosition, { x: offsetRotation.x, y: -(offsetRotation.y - 0.85), z: offsetRotation.z, w: offsetRotation.w } ); 
+	// const teleportSpaceOffset = baseReferenceSpace.getOffsetReferenceSpace( transform );
+	// renderer.xr.setReferenceSpace( teleportSpaceOffset );
+});
